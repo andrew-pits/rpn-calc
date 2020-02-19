0cc48ba1ddf7c70d0d68728c4c9a15d2b0a15496
Andrew Pits <andrii.pits@skydreams.nl>
2020-02-19 15:35:53 +0200

    Implement console input/output

---
+++ Console/Input.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AndrewPits\Calc\Console;
+
+use AndrewPits\Calc\InputInterface;
+
+class Input implements InputInterface
+{
+    public function read(): string
+    {
+        $input = readline('> ');
+        if ($input === false) { // Ctrl + D
+            return InputInterface::END_OF_INPUT;
+        }
+
+        return $input;
+    }
+}
---
+++ Console/Output.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AndrewPits\Calc\Console;
+
+use AndrewPits\Calc\OutputInterface;
+
+class Output implements OutputInterface
+{
+    public function write(string $string): void
+    {
+        echo $string . PHP_EOL;
+    }
+}
